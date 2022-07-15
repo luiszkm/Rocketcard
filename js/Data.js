a54a5801e497eb61b87c22b9b783602be1931d24
luis <luiszkm@gmail.com>
2022-07-15 17:48:27 -0300

    add prompt

--- js/Data.js
+++ js/Data.js
@@ -3,7 +3,7 @@ import { GithubUser } from './Github.js'
 
 export function Data() {
   let infoUser = []
-  //const userGithub = prompt("digite seu nome de usuário do GitHub")
+  const userGithub = prompt("digite seu nome de usuário do GitHub")
   
   const avatar = document.querySelector('.avatar')
   const li = document.querySelectorAll('.info ul .info-container li')
@@ -14,7 +14,7 @@ export function Data() {
 
 
   async function load() {
-    const user = await GithubUser.search('')
+    const user = await GithubUser.search(userGithub)
     document.querySelector('header h1').innerHTML = `${user.login}`
     infoUser[0].innerHTML = `${user.followers} Seguidores`
     infoUser[1].innerHTML = `${user.following} Seguindo`
